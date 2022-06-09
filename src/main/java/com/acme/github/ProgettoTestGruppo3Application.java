f2fb3d6401ed0e7dd4402fcedc7c6d4873464e5b
shotto <shotto@DESKTOP-C4JLDBN.lan>
2022-06-09 15:26:36 +0200

    ProgettoTestGruppo3 esercizio test gruppo3

---
+++ src/main/java/com/acme/github/ProgettoTestGruppo3Application.java
@@ -0,0 +1,13 @@
+package com.acme.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoTestGruppo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoTestGruppo3Application.class, args);
+	}
+
+}
